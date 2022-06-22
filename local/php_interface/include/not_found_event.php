721b09246643bb6d01f83490539c9d9759272d2d
nekoma-02 <kaineko01@gmail.com>
2022-06-22 16:31:46 +0300

    ex2-93

---
+++ local/php_interface/include/not_found_event.php
@@ -0,0 +1,28 @@
+<?php
+// файл /bitrix/php_interface/init.php
+// регистрируем обработчик
+AddEventHandler("main", "OnEpilog", Array("NotFoundEvent", "pageNotFound"));
+
+class NotFoundEvent
+{
+    static function pageNotFound()
+    {
+        //echo 1232142141421421;
+        if (defined("ERROR_404") && ERROR_404 == "Y") {
+            global $APPLICATION;
+            $APPLICATION->RestartBuffer();
+            include $_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/header.php";
+            include $_SERVER["DOCUMENT_ROOT"]."/404.php";
+            include $_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/footer.php";
+            CEventLog::Add(
+                array(
+                    "SEVERRITY" => "INFO",
+                    "AUDIT_TYPE_ID" => "ERROR_404",
+                    "MODULE_ID" => "main",
+                    "DESCRIPTION" => $APPLICATION->GetCurPage()
+                )
+            );
+        }
+            
+    }
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,3 +3,6 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/active_e
     include_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/active_event.php");
 }
 
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/not_found_event.php")) {
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/not_found_event.php");
+}
