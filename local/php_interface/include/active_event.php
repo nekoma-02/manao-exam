66db7a7d236323835c2f157c64caddd4f83520c5
nekoma-02 <kaineko01@gmail.com>
2022-06-22 15:19:08 +0300

    ex2-50

---
+++ local/php_interface/include/active_event.php
@@ -0,0 +1,49 @@
+<?php
+// файл /bitrix/php_interface/init.php
+// регистрируем обработчик
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("ActiveEvent", "onBeforeIBlockElementUpdate"));
+
+define("ID_BLOCK_PRODUCT",2);
+
+class ActiveEvent
+{
+    // создаем обработчик события "OnBeforeIBlockElementUpdate"
+    static function onBeforeIBlockElementUpdate(&$arFields)
+    {
+        if ($arFields["IBLOCK_ID"] == ID_BLOCK_PRODUCT) {
+            if ($arFields["ACTIVE"] == "N") {
+                $arSelect = array(
+                    "ID", 
+                    "IBLOCK_ID", 
+                    "NAME",
+                    "SHOW_COUNTER"
+                );//IBLOCK_ID и ID обязательно должны быть указаны, см. описание arSelectFields выше
+            
+                $arFilter = array(
+                    "IBLOCK_ID"=>ID_BLOCK_PRODUCT, 
+                    "ID"=>$arFields["ID"], 
+                );
+            
+                $res = CIBlockElement::GetList(
+                    array(), 
+                    $arFilter, 
+                    false, 
+                    false, 
+                    $arSelect
+                );
+            
+                $ob = $res->Fetch();
+                 
+
+                if ($ob["SHOW_COUNTER"]>2) {
+
+                    global $APPLICATION;
+                    $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $ob["SHOW_COUNTER"] . " просмотров");
+                    return false;
+                }
+
+            
+            }
+        }
+    }
+}
