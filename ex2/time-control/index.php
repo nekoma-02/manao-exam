5792fefd00efefedf02432828410ec12744076d8
nekoma-02 <kaineko01@gmail.com>
2022-07-05 16:17:03 +0300

    ex2-88

---
+++ ex2/time-control/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+Самая нагруженная страница -&nbsp;<a href="http://manao-exam2/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp-katalogtovarov%2Findex.php">/ex2/simplecomp-katalogtovarov/index.php</a>&nbsp;- 31.20%<br>
+Работа компонента по умолчанию: 10кб &nbsp;<br>
+Работа компонента с необходимыми данными: 7кб <br>
+Разница 3 кб
+
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
